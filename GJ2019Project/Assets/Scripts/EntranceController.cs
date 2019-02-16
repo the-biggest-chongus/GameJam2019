bf09daa78707df06890bc0edbcbf070e2e086af3
Cameron <csavage5@me.com>
2019-02-16 10:42:05 -0800

    Entrance Collider is working
    - created EntranceCollider prefab
    Instructions:
     - put EntranceCollider prefab as a child of the House object
    - tag all walls you want to disappear on entrance as "wall_disappear", ensure they're on the same hierarchy level as EntranceCollider
    - place EntranceColldier near the door

---
+++ GJ2019Project/Assets/Scripts/EntranceController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntranceController : MonoBehaviour
+{
+    public string status = "outside";
+    public List<GameObject> WallList;
+    public bool hitCentre = false;
+
+    // Start is called before the first frame update
+    void Start(){
+        GameObject Parent = transform.parent.gameObject;
+
+        foreach (Transform child in Parent.transform){
+            if (child.tag == "wall_disappear"){
+                WallList.Add(child.gameObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public void Enter(){
+
+        print("in Enter()");
+
+        foreach (GameObject child in WallList){
+            child.GetComponent<Renderer>().enabled = false;
+        }
+
+    }
+
+    public void Exit(){
+        //disappear[0].GetComponent<Renderer>().enabled = true;
+        //disappear[1].GetComponent<Renderer>().enabled = true;
+
+        foreach (GameObject child in WallList)
+        {
+            child.GetComponent<Renderer>().enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        // when player hits middle collider
+        hitCentre = true;
+        print("centre is hit");
+    }
+
+}
---
+++ GJ2019Project/Assets/Scripts/Entry.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entry : MonoBehaviour
+{
+
+    public EntranceController Controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Controller = transform.parent.GetComponent<EntranceController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider coll){
+        //check if centre is hit
+        //if so, then call Controller.Enter
+        if (coll.gameObject.tag == "Player"){
+            if (Controller.hitCentre == true){
+                Controller.hitCentre = false;
+                Controller.Enter();
+                print("called Controller.Enter");
+            }
+        }
+
+    }
+}
