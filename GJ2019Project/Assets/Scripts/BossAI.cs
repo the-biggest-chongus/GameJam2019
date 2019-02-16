7d0757a9462bc61d1d4fe67e9079dad88bd183ce
JasoN <jasonkngan@gmail.com>
2019-02-16 12:58:57 -0800

    Boss can become enraged
    Boss spins when raging

---
+++ GJ2019Project/Assets/Scripts/BossAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAI : MonoBehaviour
+{
+    private bool raging = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("rage", 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (raging)
+        {
+            spinLikeCrazy();
+        } else
+        {
+
+        }
+    }
+
+    private void rage()
+    {
+        raging = true;
+    }
+
+    private void spinLikeCrazy()
+    {
+        transform.Rotate(Vector3.up * 1000f * Time.deltaTime, Space.World);
+    }
+}
