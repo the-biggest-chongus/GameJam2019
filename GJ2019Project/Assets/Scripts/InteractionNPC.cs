394614069651f97589804731c7bef70403228b50
Melissa Lee <melissaslee0@gmail.com>
2019-02-16 18:05:55 -0800

    prefab made

--- GJ2019Project/Assets/Scripts/InteractionNPC.cs
+++ GJ2019Project/Assets/Scripts/InteractionNPC.cs
@@ -6,10 +6,19 @@ public class InteractionNPC : MonoBehaviour
 {
     int happyState = 0;
     public GameObject heartParticles;
+
+
+    public GameObject attention;
+
+    private DialogueTrigger dialogueTrigger;
+    private bool activeAttention = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dialogueTrigger = this.GetComponent<DialogueTrigger>();
+        attention = this.transform.GetChild(0).gameObject;
+        attention.GetComponent<Renderer>().enabled = false;
     }
 
     // Update is called once per frame
@@ -17,15 +26,33 @@ public class InteractionNPC : MonoBehaviour
     {
         
     }
+
+    public void triggerActive()
+    {
+        activeAttention = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        attention.GetComponent<Renderer>().enabled = false;
+    }
+
     private void OnTriggerStay(Collider other)
     {
+        if (activeAttention)
+        {
+            attention.GetComponent<Renderer>().enabled = true;
+        }
+        
         if (Input.GetKeyDown(KeyCode.E))
         {
-            print("happy state");
-            happyState = 1;
-            GameObject particlePlaced = Instantiate(heartParticles, transform.position + new Vector3(0, 0.7f, 0), Quaternion.identity);
-            particlePlaced.transform.parent = gameObject.transform;
-            particlePlaced.transform.localScale = new Vector3(1, 1, 1);
+            dialogueTrigger.TriggerDialogue();
+            //print("happy state");
+            //happyState = 1;
+            //GameObject particlePlaced = Instantiate(heartParticles, transform.position + new Vector3(0, 0.7f, 0), Quaternion.identity);
+            //particlePlaced.transform.parent = gameObject.transform;
+            //particlePlaced.transform.localScale = new Vector3(1, 1, 1);
+            activeAttention = false;
         }
 
     }
