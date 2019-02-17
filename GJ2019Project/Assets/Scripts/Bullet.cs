80943f917cabbcdeb1049189697c6e4e45755a5c
Solomon <solomonyu1@gmail.com>
2019-02-16 17:37:46 -0800

    added bullet and camera

---
+++ GJ2019Project/Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Destroy(this.gameObject, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * 6f);
+    }
+}
