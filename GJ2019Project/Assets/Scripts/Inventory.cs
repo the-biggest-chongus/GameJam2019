a14501a41a35777f26b95d483201b1ed7050414b
JasoN <jasonkngan@gmail.com>
2019-02-16 21:21:43 -0800

    Player can pickup objects and add to inventory
    Added basic Inventory system

---
+++ GJ2019Project/Assets/Scripts/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField]
+    private List<Item> items;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        items = new List<Item>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addItem(Item item)
+    {
+        items.Add(item);
+    }
+
+    public bool hasItem(Item item)
+    {
+        return items.Contains(item);
+    }
+}
--- GJ2019Project/Assets/Scripts/Item.cs
+++ GJ2019Project/Assets/Scripts/Item.cs
@@ -2,28 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+public enum Item
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            if (Input.GetKeyDown(KeyCode.T))
-            {
-                Destroy(gameObject);
-            }
-        }
-    }
+    LAPTOP,
+    KEY
 }
